26436363d84a86af2991b187898df9cbae93aa84
AroniasPrenovost <Aroniascoder@gmail.com>
2016-10-06 23:23:39 -0700

    sketch.js update

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 
 function setup() {
-var canvas = createCanvas(585, 440);
+	var canvas = createCanvas(585, 440);
 // this places my canvas inside a div element
 canvas.parent("sketch-holder");
 background(72, 103, 184);
@@ -11,11 +11,11 @@ frameRate(60);
 }
 
 function draw() {
-mY = mouseY;  
-mX = mouseX;
+	mY = mouseY;  
+	mX = mouseX;
 
-stroke((300));
-line(random(mouseX), random(mouseX), 280, 170);
-fill(72, 103, 184);
+	stroke((300));
+	line(random(mouseX), random(mouseX), 280, 170);
+	fill(72, 103, 184);
 }
 
